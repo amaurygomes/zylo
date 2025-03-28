25abb2b78c1f34a36ce971ee1a9b88da5b9cd09c
Amaury Gomes <amaury_mju@hotmail.com>
2025-03-28 06:31:49 -0400

    chore: initial project setup with dependencies installed

---
+++ src/main/java/br/com/amaurygomes/zylo/ZyloApplication.java
@@ -0,0 +1,13 @@
+package br.com.amaurygomes.zylo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyloApplication.class, args);
+	}
+
+}
