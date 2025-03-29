ca17fa7d56b3de1293474207b2558c7166d8a26e
Amaury Gomes <amaury_mju@hotmail.com>
2025-03-29 12:46:58 -0400

    feat(dto): add additional validation to CreatePlanDTO fields

--- src/main/java/br/com/amaurygomes/zylo/dto/CreatePlanDTO.java
+++ src/main/java/br/com/amaurygomes/zylo/dto/CreatePlanDTO.java
@@ -2,13 +2,21 @@ package br.com.amaurygomes.zylo.dto;
 
 import br.com.amaurygomes.zylo.enums.BillingCycle;
 import br.com.amaurygomes.zylo.model.PlanModel;
-import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
 public record CreatePlanDTO(
+        @NotNull(message = "Plan name cannot be null")
+        @NotBlank(message = "Plan name cannot be blank")
         String name,
+        @NotNull(message = "Plan price cannot be null")
+        @PositiveOrZero(message = "Plan price must be greater than or equal to zero")
+        @Max(value = 10000, message = "Plan price must be less than or equal to 10,000")
         BigDecimal price,
+        @NotNull(message = "Plan billing cycle cannot be null")
+        @JsonProperty("billing_cycle")
         BillingCycle billingCycle
 ) {
 
