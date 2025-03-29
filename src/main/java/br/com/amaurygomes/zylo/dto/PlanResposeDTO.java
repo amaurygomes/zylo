7115601eaf31cc784b872ef08f4669263df807a5
Amaury Gomes <amaury_mju@hotmail.com>
2025-03-29 16:56:22 -0400

    feat(dto): add toDTO method to PlanResposeDTO

--- src/main/java/br/com/amaurygomes/zylo/dto/PlanResposeDTO.java
+++ src/main/java/br/com/amaurygomes/zylo/dto/PlanResposeDTO.java
@@ -1,10 +1,13 @@
 package br.com.amaurygomes.zylo.dto;
 
 import br.com.amaurygomes.zylo.enums.BillingCycle;
+import br.com.amaurygomes.zylo.model.PlanModel;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Builder
 public record PlanResposeDTO(
         UUID id,
         String name,
@@ -12,4 +15,15 @@ public record PlanResposeDTO(
         BillingCycle billingCycle,
         Boolean active
 ) {
+
+    public static PlanResposeDTO toDTO(br.com.amaurygomes.zylo.model.PlanModel planModel){
+        return PlanResposeDTO.builder()
+                .id(planModel.getId())
+                .name(planModel.getName())
+                .price(planModel.getPrice())
+                .billingCycle(planModel.getBillingCycle())
+                .active(planModel.isActive())
+                .build();
+    }
+
 }
