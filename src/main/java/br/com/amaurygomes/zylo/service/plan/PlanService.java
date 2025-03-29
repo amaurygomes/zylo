b56507f1f82fd9e264216479382030eb9419fb6c
Amaury Gomes <amaury_mju@hotmail.com>
2025-03-29 10:49:54 -0400

    feat(service): add parameters to enhance functionality in PlanService

--- src/main/java/br/com/amaurygomes/zylo/service/plan/PlanService.java
+++ src/main/java/br/com/amaurygomes/zylo/service/plan/PlanService.java
@@ -1,13 +1,15 @@
 package br.com.amaurygomes.zylo.service.plan;
 
+import br.com.amaurygomes.zylo.dto.CreatePlanDTO;
 import br.com.amaurygomes.zylo.dto.PlanResposeDTO;
+import br.com.amaurygomes.zylo.dto.UpdatePlanDTO;
 
 import java.util.List;
 
 public interface PlanService {
-    void createPlan();
-    PlanResposeDTO getPlanById();
+    void createPlan(CreatePlanDTO createPlan);
+    PlanResposeDTO getPlanById(String id);
     List<PlanResposeDTO> getPlans();
-    void updatePlan();
-    void deletePlan();
+    void updatePlan(String id, UpdatePlanDTO updatePlan);
+    void deletePlan(String id);
 }
