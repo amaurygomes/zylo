342ef734a947dfd9ac9804283834fe515f157fde
Amaury Gomes <amaury_mju@hotmail.com>
2025-03-30 18:46:30 -0400

    refactor(dto): apply @JsonProperty to align billingCycle field with JSON format

--- src/main/java/br/com/amaurygomes/zylo/dto/PlanResposeDTO.java
+++ src/main/java/br/com/amaurygomes/zylo/dto/PlanResposeDTO.java
@@ -2,6 +2,7 @@ package br.com.amaurygomes.zylo.dto;
 
 import br.com.amaurygomes.zylo.enums.BillingCycle;
 import br.com.amaurygomes.zylo.model.PlanModel;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -12,11 +13,12 @@ public record PlanResposeDTO(
         UUID id,
         String name,
         BigDecimal price,
+        @JsonProperty("billing_cycle")
         BillingCycle billingCycle,
         Boolean active
 ) {
 
-    public static PlanResposeDTO toDTO(br.com.amaurygomes.zylo.model.PlanModel planModel){
+    public static PlanResposeDTO toDTO(PlanModel planModel){
         return PlanResposeDTO.builder()
                 .id(planModel.getId())
                 .name(planModel.getName())
